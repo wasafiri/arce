c740c2fa150410afd6158133127e97d5357ca9b2
David Bahar <wasafiri@users.noreply.github.com>
2016-02-04 12:21:51 -0500

    Refactor script; eliminate ghost texts
    Prior version of script turned lights on + sent texts when board powered up. That no longer happens here.

--- arce.ino
+++ arce.ino
@@ -1,7 +1,10 @@
-// Hue network ip and access info
+// Hue variables
 TCPClient client;
-const char hueHubIP[] = "192.168.1.20";     // Local HUE bridge
-const char hueUsername[] = "newdeveloper";  // Hue username
+const char hueHubIP[] = "192.168.1.16";     // local HUE bridge
+const int hueHubPort = 80;                  // local HUE bridge port
+const char hueUsername[] = "newdeveloper";  // hue username
+int lightNum;                               // which HUE light we want to control
+unsigned int len;                           // to store length of String command sent to HUE
 
 // read the buzzer value
 int buzzerPin = D3;                         // pin the buzzer is connected to
@@ -12,25 +15,14 @@ long lastDebounceTime = 0;                  // the last time the output pin was 
 long debounceDelay = 120;                   // the debounce time; increase if multiple texts are sent
 int lastBuzzerState = HIGH;                 // store the previous reading from the buzzerPin
 
-// Hue variables
-int lightNum;                               // which HUE light we want to control
-unsigned int len;                           // to store length of String command sent to HUE
-
 void setup() {
     // read buzzerPin
     pinMode(buzzerPin, INPUT_PULLUP);
     
-    // set up connection to HUE bridge
+    // turn on serial communication
     Serial.begin(9600);
-    delay(1000);
-    client.connect(hueHubIP, 80);
-    delay(1000);
-    if (client.connected()){
-        Serial.println("connection ready.");
-    }
-    else{
-        Serial.println("connection failed");
-    }
+    delay(200);
+    Serial.println("setup done");
 }
 
 void loop() {
@@ -49,28 +41,28 @@ void loop() {
     // than the debounce delay, so take it as the actual current state:
     if ((millis() - lastDebounceTime) > debounceDelay) {
         
-        // if the actual current state of the buzzer has changed,
-        // save it to the buzzerState variable
+        // change saved buzzer state to buzzerState variable
         if (reading != buzzerState) {
             buzzerState = reading;
             
             // if the buzzerState is LOW, send a text via Twilio    
-            if (buzzerState == LOW);
+            if (buzzerState == LOW) {
+                Serial.print(buzzerState);
                 // use a webhook to send json to Twilio
                 Particle.publish("twilio", "Heads up: Someone just buzzed your intercom!", 60, PRIVATE);
-                
+            
                 // if a connection to the HUE bridge exists then do stuff with the lights
-                if (client.connect(hueHubIP, 80)) {
-                    while (client.connected())
-                    {
-                        String command = "{\"on\": true,\"hue\":50000,\"sat\":254,\"bri\":180,\"alert\":\"none\",\"transitiontime\":"+String(random(40,120))+"}";
-                        setHue(lightNum, command);
-                        delay(8000);
-                    }
-                }
+                String command = "{\"on\": true,\"hue\":50000,\"sat\":254,\"bri\":180,\"alert\":\"none\",\"transitiontime\":40}";
+                setHue(lightNum, command);
+                delay(12000);
+                // turning HUE off after alert produces nicer gradual off than by relying on 'else' to turn it off
+                command = "{\"on\": false}";
+                setHue(lightNum, command);
+                delay(50);
+            }
         }
         else {
-            // turn HUE off
+            // turn HUE off if for some reason it's on, else it will stay on until someone pushes the buzzer
             String command = "{\"on\": false}";
             setHue(lightNum, command);
         }
@@ -79,28 +71,24 @@ void loop() {
     lastBuzzerState = reading;
 }
 
-boolean setHue(int lightNum, String command)        // Moving this out of the loop
-{
-    if (client.connect(hueHubIP, 80))
-    {
-        if(client.connected())
-        {
-            unsigned int len = command.length();    //get length
-            client.println("PUT /api/newdeveloper/lights/3/state HTTP/1.1");
-            client.println("Connection: keep-alive");
-            client.print("Host: ");
-            client.println(hueHubIP);
-            client.println("Content-Type: text/plain;charset=UTF-8"); //
-            client.print("Content-Length: ");
-            client.println(len);                    //brightness string + val length
-            client.println();                       // blank line before body
-            client.print(command);
-            Serial.print("sent");                   // command executed
-            delay(100);                             // slight delay IMPORTANT
-        }
+boolean setHue(int lightNum, String command) {      // moving this out of the loop
+    if (client.connect(hueHubIP, hueHubPort)) {
+        Serial.println("hue bridge connected");
+        unsigned int len = command.length();        // get length
+        client.println("PUT /api/newdeveloper/lights/3/state HTTP/1.1");
+        client.println("Connection: keep-alive");
+        client.print("Host: ");
+        client.println(hueHubIP);
+        client.println("Content-Type: text/plain;charset=UTF-8");
+        client.print("Content-Length: ");
+        client.println(len);                        // brightness string + val length
+        client.println();                           // blank line before body
+        client.print(command);
+        Serial.println("command sent to HUE ");     // command executed
+        delay(100);                                 // slight delay IMPORTANT
         client.stop();
-        return true;                                // command executed
     }
-    else
-    return false;                                   // command failed
+    else {
+        Serial.println("hue bridge not connected");
+    }
 }
